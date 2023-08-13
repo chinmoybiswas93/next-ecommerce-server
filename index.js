0f88dd8f3acd96db6f82fc56f43355d5c14cb9c9
Chinmoy Biswas <chinmoy709@gmail.com>
2023-08-13 13:54:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Define your API routes here
+app.post("/api/user-registration", (req, res) => {
+  res.status(201).json({ message: "User registered successfully" });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
